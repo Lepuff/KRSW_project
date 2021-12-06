68cc67c71556b7502ce82d94c05a852625fb0664
Samuel <48945979+svsa1721@users.noreply.github.com>
2021-12-06 15:49:59 +0100

    Fixed something

--- dbpedia_query.py
+++ dbpedia_query.py
@@ -1,37 +1,61 @@
-# 
-# A simple example program for connecting and querying the Linked Data in Python.
-#
-# by  He Tan, 2021-10-14
-#
+
 
 from typing import List
 import stardog
-import json
 import re
 from datetime import date
+import musicbrainzngs
+#General stuff
+musicbrainzngs.set_useragent("KSRW_project","1.0",contact=None)
+musicbrainzngs.set_hostname("musicbrainz.org",use_https=False)
 
-"""
-#connect to the local stardog triplestore server
-conn_details = {
-  'endpoint': 'http://localhost:5820',
-  'username': 'admin',
-  'password': 'admin'
-}
 
-#name of the datbase to query
-db = "your_database"
+def getReleasesByMbID(artist_mbid):
+    #label = "71247f6b-fd24-4a56-89a2-23512f006f0c"
+    #label ="b95ce3ff-3d05-4e87-9e01-c97b66af13d4"
+    limit = 100
+    offset = 0
+    releases = []
+    page = 1
+    #print("fetching page number %d.." % page)
+    result = musicbrainzngs.browse_releases(artist=artist_mbid,
+                        release_type=["album","single"],release_status=["official"],includes=["media"], limit=limit, offset=offset)
+    page_releases = result['release-list']
+    releases += page_releases
+    # release-count is only available starting with musicbrainzngs 0.5
+    if "release-count" in result:
+            count = result['release-count']
+            print("")
 
-"""
+    while len(page_releases) >= limit:
+        offset += limit
+        page += 1
+        # print("fetching page number %d.." % page)
+        result = musicbrainzngs.browse_releases(artist=artist_mbid,
+                            release_type=["album","single"],release_status=["official"],includes=["media"], limit=limit, offset=offset)
+        page_releases = result['release-list']
+        releases += page_releases
+    print("")
+
+    releases_list =[]
+    u_list = []
+    for r in releases:
+        trackcount = r["medium-list"][0]["track-count"] 
+        #print("id : "+r["id"]+"   title: "+r["title"] + " track count : "+str(trackcount))
+        if r["title"] not in u_list:
+            u_list.append(r["title"])
+            releases_list.append((r["id"],r["title"],trackcount))#r["date"],trackcount))
+    return releases_list
+
+
 
 #connect to the DBpedia endpoint 
 conn_details = {
   'endpoint': 'https://dbpedia.org/'
 }
 
-
 #name of the datbase to query
 db = "sparql"
-
 
 
 #connect to the database
@@ -66,7 +90,7 @@ with stardog.Connection(db, **conn_details) as conn:
 #The SPARQL query result is serialized in a JSON format. 
 #The format details can be found at https://www.w3.org/TR/sparql11-results-json/
 # 
-myList = []
+
 for result in results["results"]["bindings"]:
     #myList.append(result["name"]["value"])
     musicbrainzID = re.split(r'http://musicbrainz.org/artist/', result["musicBrainz"]["value"])
@@ -97,4 +121,8 @@ for result in results["results"]["bindings"]:
     print('Years active:', yearsActive)
     #print(about)
     #print('years active:', (int(date.year())-int(result["startYear"]["value"])))
-  # print(test[1]) prints the musicbrainz id
+
+releases_list = getReleasesByMbID(musicbrainzID[1])
+for rel in releases_list:
+  print(rel)
+
