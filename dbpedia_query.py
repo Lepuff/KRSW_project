0c31de02894f7b5c3a067ad2dcdffa3f56fdec7a
Samuel <48945979+svsa1721@users.noreply.github.com>
2021-12-07 08:26:52 +0100

    Fixed indentation

--- dbpedia_query.py
+++ dbpedia_query.py
@@ -11,40 +11,42 @@ musicbrainzngs.set_hostname("musicbrainz.org",use_https=False)
 
 
 def getReleasesByMbID(artist_mbid):
-    #label = "71247f6b-fd24-4a56-89a2-23512f006f0c"
-    #label ="b95ce3ff-3d05-4e87-9e01-c97b66af13d4"
-    limit = 100
-    offset = 0
-    releases = []
-    page = 1
-    #print("fetching page number %d.." % page)
-    result = musicbrainzngs.browse_releases(artist=artist_mbid,
-                        release_type=["album","single"],release_status=["official"],includes=["media"], limit=limit, offset=offset)
-    page_releases = result['release-list']
-    releases += page_releases
-    # release-count is only available starting with musicbrainzngs 0.5
-    if "release-count" in result:
-            count = result['release-count']
-            print("")
+  #label = "71247f6b-fd24-4a56-89a2-23512f006f0c"
+  #label ="b95ce3ff-3d05-4e87-9e01-c97b66af13d4"
+  limit = 100
+  offset = 0
+  releases = []
+  page = 1
+  #print("fetching page number %d.." % page)
+  result = musicbrainzngs.browse_releases(artist=artist_mbid,
+                                          release_type=["album","single"],release_status=["official"],
+                                          includes=["media"], limit=limit, offset=offset)
+  page_releases = result['release-list']
+  releases += page_releases
+  # release-count is only available starting with musicbrainzngs 0.5
+  if "release-count" in result:
+    count = result['release-count']
+    
 
     while len(page_releases) >= limit:
-        offset += limit
-        page += 1
-        # print("fetching page number %d.." % page)
-        result = musicbrainzngs.browse_releases(artist=artist_mbid,
-                            release_type=["album","single"],release_status=["official"],includes=["media"], limit=limit, offset=offset)
-        page_releases = result['release-list']
-        releases += page_releases
-    print("")
+      offset += limit
+      page += 1
+      # print("fetching page number %d.." % page)
+      result = musicbrainzngs.browse_releases(artist=artist_mbid,
+                                              release_type=["album","single"],
+                                              release_status=["official"],includes=["media"], 
+                                              limit=limit, offset=offset)
+      page_releases = result['release-list']
+      releases += page_releases
 
     releases_list =[]
     u_list = []
     for r in releases:
-        trackcount = r["medium-list"][0]["track-count"] 
-        #print("id : "+r["id"]+"   title: "+r["title"] + " track count : "+str(trackcount))
-        if r["title"] not in u_list:
-            u_list.append(r["title"])
-            releases_list.append((r["id"],r["title"],trackcount))#r["date"],trackcount))
+      trackcount = r["medium-list"][0]["track-count"] 
+      #print("id : "+r["id"]+"   title: "+r["title"] + " track count : "+str(trackcount))
+      if r["title"] not in u_list:
+        u_list.append(r["title"])
+        releases_list.append((r["id"],r["title"],trackcount))#r["date"],trackcount))
     return releases_list
 
 
@@ -62,18 +64,18 @@ db = "sparql"
 with stardog.Connection(db, **conn_details) as conn:
     #query
     #####update the query as needed 
-    name = input("SAY MY NAME:\n")
-    results = conn.select("""
-    select distinct *
-    where 
-    {
-      ?artist foaf:name ?name ;
-        owl:sameAs ?musicBrainz ;
-        dbo:activeYearsStartYear ?startYear ;
-        dbo:abstract ?about .
-        optional { ?artist dbo:activeYearsEndYear ?endYear .}
+  name = input("SAY MY NAME:\n")
+  results = conn.select("""
+  select distinct *
+  where 
+  {
+    ?artist foaf:name ?name ;
+      owl:sameAs ?musicBrainz ;
+      dbo:activeYearsStartYear ?startYear ;
+      dbo:abstract ?about .
+      optional { ?artist dbo:activeYearsEndYear ?endYear .}
 
-      OPTIONAL {  ?artist dbo:birthDate ?birthDate .
+      optional {  ?artist dbo:birthDate ?birthDate .
                   ?artist dbo:birthYear ?birthYear .
                   ?artist dbo:deathDate ?deathDate . 
                   ?artist dbo:deathYear ?deathYear .}
@@ -92,23 +94,23 @@ with stardog.Connection(db, **conn_details) as conn:
 # 
 
 for result in results["results"]["bindings"]:
-    #myList.append(result["name"]["value"])
-    musicbrainzID = re.split(r'http://musicbrainz.org/artist/', result["musicBrainz"]["value"])
-    about = result["about"]["value"]
-    name = result["name"]["value"]
-    print('Name is:', name)
-    age = date.today().year
+  #myList.append(result["name"]["value"])
+  musicbrainzID = re.split(r'http://musicbrainz.org/artist/', result["musicBrainz"]["value"])
+  about = result["about"]["value"]
+  name = result["name"]["value"]
+  print('Name is:', name)
+  age = date.today().year
 
-    if("birthDate" in result):
-      birthDate = result["birthDate"]["value"]
-      print('Birth date is:', birthDate)
-      birthYear = int(result["birthYear"]["value"])
-      if ("deathDate" in result):
-        deathDate = result["deathDate"]["value"]
-        print('Date of death is:', deathDate)
-        age = int(result["deathYear"]["value"])
-      age -= birthYear
-      print(f"Age: {age} years old")
+  if("birthDate" in result):
+    birthDate = result["birthDate"]["value"]
+    print('Birth date is:', birthDate)
+    birthYear = int(result["birthYear"]["value"])
+    if ("deathDate" in result):
+      deathDate = result["deathDate"]["value"]
+      print('Date of death is:', deathDate)
+      age = int(result["deathYear"]["value"])
+    age -= birthYear
+    print(f"Age: {age} years old")
 
     startYear = int(result["startYear"]["value"])
     print('Start year is:', startYear)
@@ -122,6 +124,7 @@ for result in results["results"]["bindings"]:
     #print(about)
     #print('years active:', (int(date.year())-int(result["startYear"]["value"])))
 
+# musicbrainzID is split using regex, which is why the ID is [1]
 releases_list = getReleasesByMbID(musicbrainzID[1])
 for rel in releases_list:
   print(rel)
